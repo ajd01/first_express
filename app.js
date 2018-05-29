8f2556ed0e17d6a3be336fe7163f94a88b83ebb6
Audelio <audelio.l@gmial.com>
2018-05-29 11:43:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+const data = [
+    "First Data",
+    "Second Data"
+]
+
+app.get('/',(req,res)=> {
+    let ul = "<ul>"
+    ul += data.map((v)=>"<li>"+v+"</li>")
+    ul += "</ul>"
+    let form = "<form action='/add' method='POST'>"+
+                    "<input type='text' name='info'/>"+
+                    "<input type='submit' name='submit' value='Add'/>"+
+               "</form>"
+    res.send("<h1>Hellow World!</h1> <br/>My first express App<br/>"+ul+form)
+})  
+
+app.post('/add',(req,res)=> {
+    console.log(req)
+    data.push(req.body.info)
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+})
+
+app.listen(3000, ()=> console.log("My first Exoress App running on port 3000"))
